13bd019a3256e57188117276012abe7170505f38
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-08 08:18:57 -0600

    Removed some debugging code.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,14 +11,6 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
 
-        $salt = "";
-        for ($i = 0; $i < 22; $i++) {
-            $salt .= substr("./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", mt_rand(0, 63), 1);
-        }
-
-        var_dump(strlen(sha1('blah')));
-
-        die();
     }
 
 }
