b33eb8c99825cee3e6218f7af525876af2cb8eb4
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-09 00:15:03 -0600

    Adds comments, minor corrections

--- application/views/helpers/LoggedInAs.php
+++ application/views/helpers/LoggedInAs.php
@@ -1,5 +1,19 @@
 <?php
+/**
+ * Jeera Trouble Ticket System
+ *
+ * @category    Jeera
+ * @package     Jeera_View
+ * @subpackage  Helper
+ */
 
+/**
+ * View helper to provide welcome message and logout link
+ *
+ * @category    Jeera
+ * @package     Jeera_View
+ * @subpackage  Helper
+ */
 class Jeera_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
 {
 
@@ -18,7 +32,9 @@ class Jeera_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
                         'module' => 'default',
                         'controller' => 'auth',
                         'action' => 'logout'
-                    )
+                    ),
+                null,
+                true
             );
             $links = '<p id="logout">Howdy %s! | <a href="%s">Logout</a></p>';
             return sprintf($links, $userName, $logoutUrl);
@@ -28,17 +44,17 @@ class Jeera_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
         $controller = $request->getControllerName();
         $action = $request->getActionName();
 
-        if ($controller == 'auth' && ($action == 'index' || $action == 'register')) {
+        if ($controller == 'auth') {
             return '';
         }
 
         $loginUrl = $this->view->url(
-                    array(
-                        'module' => 'default',
-                        'controller' => 'auth',
-                        'action' => 'login'
-                    )
-            );
+                array(
+                    'module' => 'default',
+                    'controller' => 'auth',
+                    'action' => 'login'
+                )
+        );
 
         $links = '<p id="slgnIn"><a href="%s">Login</a>';
         return sprintf($links, $loginUrl);
