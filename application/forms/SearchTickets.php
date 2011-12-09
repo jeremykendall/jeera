3e59687b9b5225ba92ddf2546477d50433230b58
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-09 00:16:35 -0600

    Search form

--- application/forms/SearchTickets.php
+++ application/forms/SearchTickets.php
@@ -1,15 +1,68 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/**
+ * Jeera Trouble Ticket System
+ *
+ * @category    Jeera
+ * @package     Jeera_Form
  */
 
 /**
- * Description of SearchTickets
+ * Search form
  *
- * @author jeremykendall
+ * @category    Jeera
+ * @package     Jeera_Form
  */
 class Jeera_Form_SearchTickets extends Zend_Form
 {
-    //put your code here
+
+    public function init()
+    {
+
+        $this->addElement('text', 'ticketId', array(
+            'label' => 'Ticket Id',
+            'validators' => array('Int')
+        ));
+
+        $this->addElement('text', 'textSearch', array(
+            'label' => 'Search text'
+        ));
+
+        $this->addElement('select', 'status', array(
+            'required' => true,
+            'label' => 'Status'
+        ));
+
+        $this->addElement('select', 'createdBy', array(
+            'required' => true,
+            'label' => 'Created By'
+        ));
+
+        $this->addElement('select', 'assignedTo', array(
+            'required' => true,
+            'label' => 'Assigned To'
+        ));
+        
+        $this->addElement('select', 'impact', array(
+            'required' => true,
+            'label' => 'Impact'
+        ));
+
+        $this->addElement('select', 'lastUpdatedBy', array(
+            'required' => true,
+            'label' => 'Last Updated By'
+        ));
+
+        $this->addElement('submit', 'submit', array(
+            'label' => 'Search',
+            'ignore' => true,
+            'order' => 1000
+        ));
+
+        $this->setElementFilters(array(
+            new Zend_Filter_StringTrim(),
+            new Zend_Filter_StripTags()
+        ));
+    }
+
 }
