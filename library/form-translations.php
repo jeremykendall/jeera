6f1f89aa59cec4ae68834d7622c1c725fd756a00
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-09 10:22:37 -0600

    Translation file for use with forms.

---
+++ library/form-translations.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Create custom form validation messages by matching the error key to a
+ * message of your choosing.
+ * 
+ * @see http://framework.zend.com/manual/en/zend.form.i18n.html
+ */
+
+return array(
+    'isEmpty' => 'This is a required field',
+    'notInArray' => 'Please make a valid selection'
+);
