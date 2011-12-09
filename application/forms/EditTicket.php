0a3304f083ae0f325c27a3fd35e3b54e01fa1ba5
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-08 22:14:57 -0600

    Application forms

--- application/forms/EditTicket.php
+++ application/forms/EditTicket.php
@@ -8,13 +8,30 @@
  */
 
 /**
- * New trouble ticket form
+ * Edit trouble ticket form
  *
  * @category    Jeera
  * @package     Jeera_Form
  */
-class Jeera_Form_NewTicket extends Zend_Form
+class Jeera_Form_EditTicket extends Zend_Form
 {
+
+    /**
+     * @var array List of all application admin users
+     */
+    private $_adminUsers;
+
+    /**
+     * Create new Jeera_Form_EditTicket form
+     *
+     * @param array $adminUsers List of all application admin users
+     * @param mixed $options
+     */
+    public function __construct(array $adminUsers, $options = null)
+    {
+        $this->_adminUsers = $adminUsers;
+        parent::__construct($options);
+    }
 
     public function init()
     {
@@ -22,12 +39,12 @@ class Jeera_Form_NewTicket extends Zend_Form
             'validators' => array('Int'),
             'filters' => array('Int'),
         ));
-        
+
         $this->addElement('hidden', 'lastUpdatedBy', array(
             'validators' => array('Int'),
             'filters' => array('Int'),
         ));
-        
+
         $this->addElement('text', 'problemType', array(
             'required' => true,
             'label' => 'Problem Type'
@@ -38,6 +55,18 @@ class Jeera_Form_NewTicket extends Zend_Form
             'label' => 'Problem Description',
             'cols' => 40,
             'rows' => 3,
+        ));
+
+        $statusOptions = array(
+            'new' => 'new',
+            'in-progress' => 'in-progress',
+            'complete' => 'complete'
+        );
+
+        $this->addElement('select', 'status', array(
+            'multiOptions' => $statusOptions,
+            'required' => true,
+            'label' => 'Status'
         ));
 
         $impactOptions = array(
@@ -53,6 +82,22 @@ class Jeera_Form_NewTicket extends Zend_Form
             ),
             'required' => true,
             'label' => 'Impact'
+        ));
+
+        $this->addElement('select', 'assignedTo', array(
+            'multiOptions' => array('Select assignee') + $this->_adminUsers,
+            'validators' => array(
+                array('InArray', false, array(array_keys($this->_adminUsers)))
+            ),
+            'required' => true,
+            'label' => 'Assign To'
+        ));
+
+        $this->addElement('textarea', 'notes', array(
+            'required' => true,
+            'label' => 'Notes',
+            'cols' => 40,
+            'rows' => 3,
         ));
 
         $this->addElement('submit', 'submit', array(
